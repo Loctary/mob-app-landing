21a56b734cfed6a9fdac58787a36426b7be7a169
Vasyl Butov <loctary@mail.ru>
2019-08-05 17:12:21 +0300

    Updates

---
+++ precache-manifest.087944b6397de9e323195ee4ebdeb8b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b837b2232f95f76800668d742d2b3db1",
+    "url": "/mob-app-landing/index.html"
+  },
+  {
+    "revision": "39fee049226be38dcea1",
+    "url": "/mob-app-landing/static/js/2.86885398.chunk.js"
+  },
+  {
+    "revision": "9a7a3761432a60ad696c",
+    "url": "/mob-app-landing/static/js/main.2fa460e7.chunk.js"
+  },
+  {
+    "revision": "53c502d5f3295e29fcb2",
+    "url": "/mob-app-landing/static/js/runtime~main.b1d8a9ab.js"
+  },
+  {
+    "revision": "9e4c1e3ad8546db35143d6aa4f71948a",
+    "url": "/mob-app-landing/static/media/iphonex.9e4c1e3a.png"
+  }
+]);
